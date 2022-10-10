5c454b72578fe889ba513a1b81e9107adc9cfc00
Md Galib Hossain <mdgalib23@gmail.com>
2022-10-11 03:16:07 +0600

    api loaded

---
+++ src/Components/Options/Options.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Options.css";
+
+const Options = ({ option }) => {
+  return (
+    <div className="col-lg-6 col-sm-12 option hover ">
+      <p>{option}</p>
+    </div>
+  );
+};
+
+export default Options;
+// kaj kortisilam
--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Options from "../Options/Options";
+import "./question.css";
 
-const Question = () => {
-  return <div className="quesDiv"></div>;
+const Question = (props) => {
+  const { options, question, correctAnswer } = props.question;
+
+  return (
+    <div className="quesDiv container">
+      <div className="row">
+        <div className="col-12">
+          <p>{question}</p>
+        </div>
+        {options.map((option) => (
+          <Options key={option.id} option={option}></Options>
+        ))}
+      </div>
+    </div>
+  );
 };
-
+// kaj kortisilam
 export default Question;
